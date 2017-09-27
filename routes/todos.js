c9e8a2799d6a94288c9e994be4a8315b1b2943da
Brett Jenken <brettjenken@hotmail.com>
2017-09-26 20:55:25 -0400

    Added REST Api for mLab instance

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://admin:admin@ds149974.mlab.com:49974/meantodos', ['todos']);
 
+// get todos
 router.get('/todos', function(req, res, next){
-    res.send('TODOS API');
+    db.todos.find(function(err, todos){
+        if (err){
+            res.send(err);
+        }else{
+            res.json(todos);
+        }
+    });
+});
+
+// get single todo
+router.get('/todo/:id', function(req, res, next){
+    db.todos.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function(err, todo){
+        if (err){
+            res.send(err);
+        }else{
+            res.json(todo);
+        }
+    });
+});
+
+// save todo
+router.post('/todo', function(req, res, next){
+    var todo = req.body;
+    if(!todo.text || !(todo.isCompleted + '')){
+        res.status(400);
+        res.json({
+            "error" : "invalid data"
+        });
+    } else {
+        db.save(todo, function(err, result){
+            if (err){
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
+    }
 });
 
 module.exports = router;
